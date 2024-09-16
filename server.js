6dc5f6b39116a8b51a2d0e2307318b33fa4f3976
Shivansh <shivanshdev10@gmail.com>
2024-09-16 23:03:47 +0530

    Routing

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express(); // Create an instance of the Express application
 const bodyParser = require('body-parser'); // Import body-parser to parse incoming request bodies
 const db = require('./db'); // Import the database setup file (make sure this sets up your database connection properly)
 const Person = require('./Model/person'); // Import the Person model from the Model directory
+const userRoutes = require('./Routes/userRoutes')
 
 // Middleware
 app.use(bodyParser.json()); // Use body-parser middleware to parse JSON request bodies
@@ -20,20 +21,6 @@ app.get('/admin', (req, res) => {
   res.send('Welcome admin'); // Send a welcome message specifically for admin users
 });
 
-// Route for handling POST requests to '/user'
-app.post('/user', async (req, res) => {
-  try {
-    const data = req.body; // Extract the request body, which should include { name, age, email }
-    const newPerson = new Person(data); // Create a new Person instance with the extracted data
-
-    const response = await newPerson.save(); // Save the new person to the database and wait for the operation to complete
-    console.log('Data saved:', response); // Log the response to the console for debugging
-    res.status(200).json(response); // Send a JSON response with status 200 (OK) and include the saved data
-  } catch (error) {
-    console.log('Error saving data:', error); // Log any errors that occur during the save operation
-    res.status(500).json({ error: 'Internal server error' }); // Send a JSON response with status 500 (Internal Server Error) and include an error message
-  }
-});
 
 // Route for handling GET requests to '/user'
 app.get('/user', async (req, res) => {
@@ -46,7 +33,7 @@ app.get('/user', async (req, res) => {
     res.status(500).json({ error: 'Internal server error' }); // Send a JSON response with status 500 (Internal Server Error) and include an error message
   }
 });
-
+app.use('/user',userRoutes);
 // Start the server and listen on port 3000
 app.listen(3000, () => {
   console.log('Server started on http://localhost:3000'); // Log a message indicating that the server has started successfully
